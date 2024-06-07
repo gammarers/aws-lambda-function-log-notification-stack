efc569651f496f6853f3d5e4ffd0514d00eb87eb
yicr <yicr@users.noreply.github.com>
2024-06-07 22:20:55 +0900

    chore(test): add test

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,5 +1,20 @@
-import { Hello } from '../src';
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { LambdaFunctionLogNotificationStack } from '../src';
 
 test('hello', () => {
-  expect(new Hello().sayHello()).toBe('hello, world!');
+  const app = new App();
+
+  const stack = new LambdaFunctionLogNotificationStack(app, 'LambdaFunctionLogNotificationStack', {
+    notifications: {
+      emails: [
+        'foo@example.com',
+      ],
+    },
+    logGroupName: 'example-function-log',
+  });
+
+  const template = Template.fromStack(stack);
+
+  expect(template.toJSON()).toMatchSnapshot();
 });
