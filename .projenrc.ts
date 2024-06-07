23663b074f0311415a564d3e4286dafa981a5447
yicr <yicr@users.noreply.github.com>
2024-06-07 23:09:26 +0900

    chore(deps): change cdk version

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
   authorOrganization: true,
-  cdkVersion: '2.100.0',
+  cdkVersion: '2.120.0',
   defaultReleaseBranch: 'main',
   typescriptVersion: '5.3.x',
   jsiiVersion: '5.3.x',
