51bcf617bc490f78bbb415646414642670f6c014
yicr <yicr@users.noreply.github.com>
2024-06-08 13:51:32 +0900

    fix: log decode in function

--- src/funcs/notification.lambda.ts
+++ src/funcs/notification.lambda.ts
@@ -1,5 +1,23 @@
+import { promisify } from 'util';
+import { gunzip } from 'zlib';
+import { CloudWatchLogsEvent, Context } from 'aws-lambda';
 
-export const handler = async (event:any, context: any): Promise<void> => {
-  console.log(event);
-  console.log(context);
+const gunzipAsync = promisify(gunzip);
+
+export const handler = async (event: CloudWatchLogsEvent, context: Context): Promise<void> => {
+  console.log({ event: event });
+  console.log({ context: context });
+
+  const payload = Buffer.from(event.awslogs.data, 'base64');
+
+  try {
+    const result = await gunzipAsync(payload);
+    const logData = JSON.parse(result.toString('utf8'));
+    console.log({ DecodedLog: logData });
+    // ここでlogDataを処理する
+    return logData;
+  } catch (error) {
+    console.error(null, error);
+    throw error;
+  }
 };
