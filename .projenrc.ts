23860085759e8ee9afcbaa2b6bfac88875f05f69
yicr <yicr@users.noreply.github.com>
2024-06-08 22:15:16 +0900

    chore: set deps upgrade

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { awscdk } from 'projen';
+import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
@@ -11,11 +11,21 @@ const project = new awscdk.AwsCdkConstructLibrary({
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers/aws-lambda-function-log-notification-stack.git',
   releaseToNpm: false, // temporary
-  depsUpgrade: false, // temporary
+  depsUpgrade: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 18 * * 5']), // every sunday (JST/MON:03:00)
+    },
+  },
   devDeps: [
     '@types/aws-lambda@^8.10.136',
   ],
   minNodeVersion: '18.0.0',
   workflowNodeVersion: '22.2.0',
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['yicr'],
+  },
 });
 project.synth();
